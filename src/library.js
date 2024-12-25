946fc8559c7928f325a880d0302f662cc008e3a6
Nimesh <nimeshpansuriya719@gmail.com>
2024-12-25 21:58:40 +0530

    Passed Test Case: not able to add dullicate ISBN book

--- src/library.js
+++ src/library.js
@@ -13,13 +13,20 @@ class Library {
   // Adds a new book to the library
   addBook(book){
 
+    // Validates that the book has all required fields.
+
      if (!book.isbn || !book.title || !book.author || !book.publicationYear) {
        throw new Error(
          "All fields (ISBN, title, author, publicationYear) are required"
        );
      }
 
-     
+    // Checks if the book with the same ISBN already exists in the library.
+
+    if (this.books.some((b) => b.isbn === book.isbn)) {
+    throw new Error("The same ISBN number book is already present");
+    }
+
 
     this.books.push(book);
   }
